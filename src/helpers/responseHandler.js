96a66336a53ee8a7f00f6f6d562f1d7f233961ad
OLUWAKEMI <kemifasae@gmail.com>
2021-10-11 14:57:23 +0100

    refactor response handler

--- src/helpers/responseHandler.js
+++ src/helpers/responseHandler.js
@@ -5,73 +5,69 @@
 
  const responseHandler = (req, res, statusCode, data, message) => {
     let error
-    let errMessage
+    let msg
+    let response = data || {}
 
     switch (statusCode) {
       // Success
       case 200:
         error = false
-        errMessage = data.message || 'Success. '
+        msg = data.message || 'Success. '
         break
 
       // Resource created
       case 201:
         error = false
-        errMessage = data.message || 'Created Successfully. '
+        msg = data.message || 'Created Successfully. '
         break
       
-      // No Content
-      case 204:
-        error = true
-        errMessage = message || 'Resource not found. '
-        break
-  
+      
       //Bad Request
       case 400:
         error = true
-        errMessage = message || 'Invalid Request '
+        msg = message || 'Invalid Request '
         break
   
       // Unauthorized
       case 401:
         error = true
-        errMessage = message || 'Unauthorized Request '
+        msg = message || 'Unauthorized Request '
         break
   
       // Forbidden
       case 403:
         error = true
-        errMessage = message || 'Access to this recource is denied!'
+        msg = message || 'Access to this recource is denied!'
         break
   
       // Not Found
       case 404:
         error = true
-        errMessage = message || 'Resource not found!'
+        msg = message || 'Resource not found!'
         break
   
       //   Cannot process request
       case 422:
         error = true
-        errMessage = message || 'Invalid Request '
+        msg = message || 'Invalid Request '
         break
   
       // Internal Server Error
       case 500:
         error = true
-        errMessage = message || 'Internal server error'
+        msg = message || 'Internal server error'
         break
   
       default:
         error = true
-        errMessage = message || 'Internal server error'
+        msg = message || 'Internal server error'
         break
     }
 
-    const response = data || {}
-
+    
     response.error = error
-    response.message = errMessage
+    response.message = msg
+    
     
     return res.status(statusCode).json(response)
   }
