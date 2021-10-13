d4f31aa009d810aeefc7896edd07be615399092c
OLUWAKEMI <kemifasae@gmail.com>
2021-10-13 11:16:00 +0100

    import response handler

--- src/helpers/isAdmin.js
+++ src/helpers/isAdmin.js
@@ -1,3 +1,5 @@
+import { responseHandler } from "./responseHandler"
+
 const isAdmin = (request, response, next) => {
     if (request.user.admin !== true) {
         return responseHandler(request, response, 401,null,'You have to be an admin to do this')
